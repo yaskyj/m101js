75d4c6cb1717fb78a46c37a30391518e3e7cbf90
Justin Rogers <yaskyj@yahoo.com>
2015-01-06 20:17:48 -0600

    Add mongoclient connet code to app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 var express = require('express'),
 	cons = require('consolidate'),
-	mongodb = require('mongodb');
+	mongodb = require('mongodb'),
+	MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+	if (err) throw err;
+
+	db.collection('coll').findOne({}, function(err, doc) {
+		if (err) throw err;
+
+		console.dir(doc);
+
+		db.close();
+	});
+
+	console.dir("Called findOne!");
+});
