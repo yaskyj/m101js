6158c49c4dee7cfdab8ad58e0d172f2ed2007675
Justin Rogers <yaskyj@yahoo.com>
2015-01-07 20:17:24 -0600

    Add mongoclient

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var express = require('express'),
 	app = express(),
-	cons = require('consolidate');
+	cons = require('consolidate'),
+	MongoClient = require('mongodb').MongoClient,
+	Server = require('mongodb').Server;
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + "/views");
 
+var monngoclient = new MongoClient(new Server('localhost', 27017, {'native_parser' : true }));
+
+var db = monngoclient.db('course');
+
 app.get('/', function(req, res) {
-	res.render("hello", { 'name' : 'Swig'});
+
+	db.collection('hello_mongo_express').findOne({}, function(err, doc) {
+		res.render("hello", doc);
+	});
 });
 
 app.get('*', function(req, res) {
 	res.send("Page not found", 404);
 });
 
-app.listen(8080);
-console.log("Express server started on port 8080");
+monngoclient.open(function (err, monngoclient) {
+	if (err) throw err;
+	
+	app.listen(8080);
+	console.log("Express server started on port 8080");	
+});
