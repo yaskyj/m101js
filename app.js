ccf3360513910d4f9b720cc7bd0a8da263025c35
Justin Rogers <yaskyj@yahoo.com>
2015-01-07 18:39:19 -0600

    Add templates and swig

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	cons = require('consolidate');
+
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
 
 app.get('/', function(req, res) {
-	res.send("Hello, World!");
+	res.render("hello", { 'name' : 'Swig'});
 });
 
 app.get('*', function(req, res) {
