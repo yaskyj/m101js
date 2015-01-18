ce469c1b0652ab2092da38d9499da393f81c0c3d
yaskyj <yaskyj@yahoo.com>
2015-01-18 12:18:21 -0600

    Add initial reddit data and manipulation

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient,
+	request = require('request');
 
 MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
 	if (err) throw err;
 
-	var query = {'grade':100};
+	var query = {title:{$regex:'NSA'}};
 
-	db.collection('grades').findOne(query, function(err, doc) {
+	var projection = {title:1,_id:0};
+
+	db.collection('reddit').find(query, projection).each(function(err, doc) {
 		if (err) throw err;
 
-		console.dir(doc);
+		if (doc==null) {
+			return db.close();
+		};
 
-		db.close();
+		console.dir(doc.title);
 	});
 });
