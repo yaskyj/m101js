7e4435a7d6645abcccf4eb5e6322e090aec6f652
Justin Rogers <yaskyj@yahoo.com>
2015-01-21 15:48:44 +0000

    Add mongolab connection

--- homeworks/3.1homework.js
+++ homeworks/3.1homework.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/school', function(err, db) {
+MongoClient.connect('mongodb://admin:admin@ds031721.mongolab.com:31721/school', function(err, db) {
 	if (err) throw err;
 	db.collection('students').find({}).each(function(err, doc) {
 		if (err) throw err;
