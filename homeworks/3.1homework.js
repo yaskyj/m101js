915755982ac035b8f1bb6a18873ecdab556358a9
yaskyj <yaskyj@yahoo.com>
2015-01-20 23:12:28 -0600

    3.1 is driving me freaking nuts

--- homeworks/3.1homework.js
+++ homeworks/3.1homework.js
@@ -2,15 +2,44 @@ var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/school', function(err, db) {
 	if (err) throw err;
-
-
-	db.collection('students').find(query, projection).each(function(err, doc) {
+	db.collection('students').find({}).each(function(err, doc) {
 		if (err) throw err;
 
 		if (doc==null) {
 			return db.close();
 		};
+	
+		var lowest = 100;
 
-		console.dir(doc.title);
+		for (var i = 0; i < doc.scores.length; i++) {	
+			if (doc.scores[i]['type'] === 'homework') {
+				if (doc.scores[i]['score'] < lowest) {
+					lowest =  doc.scores[i]['score'];
+				};
+			};
+		}
+		 console.log(doc);
+		 console.log(doc['_id'])
+		 db.collection('students').findOne({'_id':doc['_id']}, function(err, result) {
+		 	if (err) throw err;
+		 	console.log(result);
+		 	return db.close();
+		 });
+		// db.collection('students').update({'_id':doc['_id']}, {$unset:{'scores.score':lowest}}, function(err, result) {
+		// db.collection('students').findOne({'_id':doc['_id']}, function(err, result) {
+
+		// 	if (err) throw err;
+		// 	console.log(result);
+		// 	// return db.close();
+		// });
 	});
+	// db.collection('students').find(query, projection).each(function(err, doc) {
+	// 	if (err) throw err;
+
+	// 	if (doc==null) {
+	// 		return db.close();
+	// 	};
+
+	// 	console.dir(doc.title);
+	// });
 });
