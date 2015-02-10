23b63695ba98f4b5f69f6099b10851399bb03b1f
yaskyj <yaskyj@yahoo.com>
2015-02-10 05:43:09 -0600

    Finish week five homework

---
+++ homeworks/hw5.js
@@ -0,0 +1,68 @@
+db.zips.aggregate([
+  {
+    $group: {
+      _id: {state:"$state", city:"$city"},
+      pop: {$sum:"$pop"}
+    }
+  },
+  { 
+    $match:{
+      $and: [
+        {pop:{$gte:25000}},{$or:[{"_id.state":"CA"}, {"_id.state":"NY"}]}
+      ]
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      avg_pop: {$avg:"$pop"}
+    }  
+  }
+]);
+
+
+db.grades.aggregate([
+  {
+    $unwind: "$scores"
+  },
+  {
+    $match:{"scores.type":{$ne:"quiz"}}
+  },
+  {
+    $group: {
+      _id: {class_id:"$class_id", student_id:"$student_id"},
+      average:{$avg:"$scores.score"} 
+    }
+  },
+  {
+    $group: {
+          _id:"$_id.class_id",
+          average:{$avg:"$average"}
+    }
+  },
+  {
+    $sort: {average:-1}
+  }
+]);
+
+db.zips.aggregate([
+  {
+    $group: {
+      _id: {state:"$state", city:"$city"},
+      pop: {$sum:"$pop"}
+    }
+  },
+  { 
+    $match:{
+      $and: [
+        {pop:{$gte:25000}},{$or:[{"_id.state":"CA"}, {"_id.state":"NY"}]}
+      ]
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      avg_pop: {$avg:"$pop"}
+    }  
+  }
+]);
