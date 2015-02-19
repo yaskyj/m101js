f02fb4954fe54375d628256e7926af9c2d544df5
yaskyj <yaskyj@yahoo.com>
2015-02-18 19:16:23 -0600

    Finish question 2

--- homeworks/final.js
+++ homeworks/final.js
@@ -1,3 +1,27 @@
 //Question 1
 db.messages.find({"headers.From": "andrew.fastow@enron.com", "headers.To": "jeff.skilling@enron.com"});
 
+//Question 2
+db.messages.aggregate([
+  {"$unwind":"$headers.To"}, 
+  {"$group": 
+    {
+      "_id":{"_id":"$_id"},
+      "From":{"$first":"$headers.From"},
+      "To":{"$addToSet":"$headers.To"}
+    }
+  },
+  {"$unwind":"$To"},
+  {"$group":
+    {
+      "_id":{
+        "From":"$From",
+        "To":"$To"
+      },
+      "count":{"$sum":1}
+    }
+  },
+  {"$sort":{"count":-1}}
+],
+  {allowDiskUse: true}
+)
